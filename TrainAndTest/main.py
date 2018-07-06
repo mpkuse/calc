07efc2b4615f99542e517bc82afbbc8485ba7f13
mpkuse <mpkuse@connect.ust.hk>
2018-07-06 11:19:08 +0800

    added simplistic experiments to get calc-descriptors for my datasets

--- TrainAndTest/main.py
+++ TrainAndTest/main.py
@@ -14,13 +14,13 @@ if __name__ == '__main__':
         import argparse
 
 	parser = argparse.ArgumentParser()
-	subparsers = parser.add_subparsers(dest='subparser')	
+	subparsers = parser.add_subparsers(dest='subparser')
 
 	## makeNet args #################
 	net_parser = subparsers.add_parser('net', help='Allows access to functions in makeNet.py, which allow for net definition, training, and descriptor dimension viewing. Run `./main.py net -h` to see all options')
 
 	# Change net defaults here
-	default_X1 = 'X1_Places' 
+	default_X1 = 'X1_Places'
 	default_X2 = 'X2_Places'
 	default_its = '500000'
 	default_batch_sz = 256
@@ -41,9 +41,9 @@ if __name__ == '__main__':
 
 	net_parser.add_argument('-v', '--view-size', dest='viewSizeFlag', action='store_true', help='If true, the descriptor dimesnions will be printed', default=False)
 
-	net_parser.add_argument('-i', '--its', dest='max_iter', nargs='?', help='The number of iterations to perform on training, default is: ' + default_its , default=default_its, type=str)        
+	net_parser.add_argument('-i', '--its', dest='max_iter', nargs='?', help='The number of iterations to perform on training, default is: ' + default_its , default=default_its, type=str)
 
-	net_parser.add_argument('-b', '--batch-sz', dest='batch_size', nargs='?', help='Batch size to train with. Deafult is: ' + str(default_batch_sz), default=default_batch_sz, type=int)        
+	net_parser.add_argument('-b', '--batch-sz', dest='batch_size', nargs='?', help='Batch size to train with. Deafult is: ' + str(default_batch_sz), default=default_batch_sz, type=int)
 
 
 	## writeDatabase args ###################################################
@@ -54,8 +54,8 @@ if __name__ == '__main__':
 
 	default_outDBNames = ['X1_Places', 'X2_Places']
 	# default for dataset to use to create database. Looks in ~/data/Places
-	defaultDirNameArr = [expanduser("~") + "/data/Places"]	
-	
+	defaultDirNameArr = [expanduser("~") + "/data/Places"]
+
 	db_parser.add_argument('-d1', '--out-db1', dest='outDBName1', nargs='?', help='The directory name with no path of the desired database name for X1. The resulting database will be written to dataRoot/train_data. Default is: '+default_outDBNames[0], default=default_outDBNames[0], type=str)
 
 	db_parser.add_argument('-d2', '--out-db2', dest='outDBName2', nargs='?', help='The directory name with no path of the desired database name for X1. The resulting database will be written to dataRoot/train_data. Default is: '+default_outDBNames[1], default=default_outDBNames[1], type=str)
@@ -65,52 +65,52 @@ if __name__ == '__main__':
 
 	db_parser.add_argument('-d', '--data-dirs', dest='dirNames', nargs='*', help='The space-separated names with full paths of the the directory (directories) containing your dataset(s). Note that if you use glob from the shell, do not use `--data-dirs=<dirs>` or else the literal string will be sent to python', default=defaultDirNameArr, type=str)
 
-	db_parser.add_argument('-db', '--debug', dest='debugFlag', action='store_true', help='Flag for whether or not to train the net with only 1 iteration for debugging', default=False) 
+	db_parser.add_argument('-db', '--debug', dest='debugFlag', action='store_true', help='Flag for whether or not to train the net with only 1 iteration for debugging', default=False)
 
-	db_parser.add_argument('-ta', '--train-after', dest='trainAfter', action='store_true', help='Flag for whether or not to train immediately after witing the database', default=False) 
+	db_parser.add_argument('-ta', '--train-after', dest='trainAfter', action='store_true', help='Flag for whether or not to train immediately after witing the database', default=False)
 
-	db_parser.add_argument('-dr', '--data-root', dest='dataRoot', nargs='?', help='Where you want the train_data directory to be placed. Default is $PWD', default="", type=str)                               
-	db_parser.add_argument('-g', '--gpu-id', dest='gpu_id', nargs='?', help='The ID of the gpu to use for training.' , default=0, type=int)        
+	db_parser.add_argument('-dr', '--data-root', dest='dataRoot', nargs='?', help='Where you want the train_data directory to be placed. Default is $PWD', default="", type=str)
+	db_parser.add_argument('-g', '--gpu-id', dest='gpu_id', nargs='?', help='The ID of the gpu to use for training.' , default=0, type=int)
 
-	db_parser.add_argument('-b', '--batch-sz', dest='batch_size', nargs='?', help='Batch size to train with. Deafult is: ' + str(default_batch_sz), default=default_batch_sz, type=int)        
+	db_parser.add_argument('-b', '--batch-sz', dest='batch_size', nargs='?', help='Batch size to train with. Deafult is: ' + str(default_batch_sz), default=default_batch_sz, type=int)
 
 	## Viewing Warped Images args ###########################################################
 
 	view_parser = subparsers.add_parser('view', help='View an example of the altered image copies')
 
-	view_parser.add_argument('file', metavar='file', help='The file of the image to warp and display', type=str)        
-	
-	view_parser.add_argument('-s', '--save', dest='save', action='store_true', help='Flag for whether or not to save the images for later use', default=False) 
-	
+	view_parser.add_argument('file', metavar='file', help='The file of the image to warp and display', type=str)
+
+	view_parser.add_argument('-s', '--save', dest='save', action='store_true', help='Flag for whether or not to save the images for later use', default=False)
+
 	## Viewing internal representations of a forward pass  ############################
 
 	pass_parser = subparsers.add_parser('pass', help='Viewing internal representations of a forward pass')
 
-	pass_parser.add_argument('file1', metavar='file1', help='The file of the first image to pass', type=str)        
-	
-	pass_parser.add_argument('file2', metavar='file2', help='The file of the second image to pass', type=str)        
+	pass_parser.add_argument('file1', metavar='file1', help='The file of the first image to pass', type=str)
+
+	pass_parser.add_argument('file2', metavar='file2', help='The file of the second image to pass', type=str)
 	## Plot text file output from vary-db-size speed tests args ##############################
 
 	plot_parser = subparsers.add_parser('plot', help='Compare varying database times between DBoW2 and DeepLCD')
 
-	plot_parser.add_argument('calc_fl', metavar='calc-fl', help='The file output from ../DeepLCD/build/vary-db-size', type=str)        
+	plot_parser.add_argument('calc_fl', metavar='calc-fl', help='The file output from ../DeepLCD/build/vary-db-size', type=str)
 
-	plot_parser.add_argument('-d', '--dbow-fl', dest='dbow_fl', help='The file output from Thirdparty/DBoW2/build/vary-db-size', type=str, default=None)        
-	
+	plot_parser.add_argument('-d', '--dbow-fl', dest='dbow_fl', help='The file output from Thirdparty/DBoW2/build/vary-db-size', type=str, default=None)
+
 
 	## Testing args ########################################################################
 
-	
+
 	test_parser = subparsers.add_parser('test', help='Allows for testing net(s) against some optional benchmark algorithms with precision-recall curves, and timing. Also allows for viewing thresholds used in those curves. Run `./main.py test -h` to see all options')
 
         test_parser.add_argument('-m', '--model', dest='model_path', nargs='*', help='The file names with full paths of the desired caffemodels for the precision-recall curve.', default="model/calc.caffemodel", type=str)
 
         test_parser.add_argument('-d', '--data-path', dest='data_path', nargs='?', help='The path to your dataset. Setup with matching images with corresponding filenames in <dataset_path>/live and <dataset_path>/memory', default="test_data/CampusLoopDataset", type=str)
 
-	test_parser.add_argument('-n', '--num-include', dest='num_include', nargs='?', help="Number of images in a sequence to count as a match. Example, if '-n5' is used, Image-7 will count as a match for Image-5 through Image-9 in the database query.", default=7, type=int) 
-	
+	test_parser.add_argument('-n', '--num-include', dest='num_include', nargs='?', help="Number of images in a sequence to count as a match. Example, if '-n5' is used, Image-7 will count as a match for Image-5 through Image-9 in the database query.", default=7, type=int)
+
 	test_parser.add_argument('-t', '--title', dest='title', nargs='?', help='Plot title', default='Precision-Recall Curve')
-	
+
 	test_parser.add_argument('-r', '--resize-net', dest='rz_net', action='store_true', help='If set to true, the net will be resized to the image size. Otherwise, the images will be resized to (120,160)', default=False)
 
         test_parser.add_argument('-ap', '--alexnet-proto', dest='alex_proto', nargs='?', help='The path to alexnet prototxt definition file', default=None, type=str)
@@ -118,7 +118,7 @@ if __name__ == '__main__':
         test_parser.add_argument('-aw', '--alexnet-weights', dest='alex_weights', nargs='?', help='The path to alexnet .caffemodel weights file', default=None, type=str)
 
 	## Parse time! #######################################################################
-	
+
 	if len(argv[1:])==0:
 		parser.print_help()
 		parser.exit()
@@ -132,7 +132,7 @@ if __name__ == '__main__':
 	elif args.subparser=='view':
 		from writeDatabase import showImWarpEx
 		showImWarpEx(args.file, args.save)
-	
+
 	elif args.subparser=='pass':
 		from testNet import view_forward_pass
 		view_forward_pass(args.file1, args.file2)
@@ -155,12 +155,7 @@ if __name__ == '__main__':
 	elif args.subparser=='test':
 		from testNet import plot
 		plot(net_model_path=args.model_path, data_path=args.data_path, num_include=args.num_include, title=args.title, resize_net=args.rz_net, alexnet_proto_path=args.alex_proto, alexnet_weights=args.alex_weights)
-	
+
 	elif args.subparser=='plot':
 		from testNet import plot_var_db_size
 		plot_var_db_size(args.calc_fl, args.dbow_fl)
-
-
-
-
-
--- TrainAndTest/testNet.py
+++ TrainAndTest/testNet.py
@@ -21,23 +21,23 @@ def smooth_pr(prec, rec):
 	n = len(prec)
 	m = 11
 	p_smooth = np.zeros((m), dtype=np.float)
-	r_smooth = np.linspace(0.0, 1.0, m) 
+	r_smooth = np.linspace(0.0, 1.0, m)
 	for i in range(m):
 		j = np.argmin( np.absolute(r_smooth[i] - rec) ) + 1
 		p_smooth[i] = np.max( prec[:j] )
-			
-	return p_smooth, r_smooth	
+
+	return p_smooth, r_smooth
 
 def check_match(im_lab_k, db_lab, num_include):
 	"""
 	Check if im_lab_k and db_lab are a match, i.e. the two images are less than or equal to
 	num_include frames apart. The correct num_include to use depends on the speed of the camera, both for frame rate as well as physical moving speed.
-	"""	
+	"""
 	if num_include == 1:
 		if db_lab ==im_lab_k:
 			return True
-	else:				
-		# This assumes that db_lab is a string of numerical characters, which it should be	
+	else:
+		# This assumes that db_lab is a string of numerical characters, which it should be
 		#print int(db_lab)-num_include/2, "<=", int(im_lab_k), "<=", int(db_lab)+num_include/2, "?"
 		if (int(db_lab)-num_include/2) <= int(im_lab_k) and int(im_lab_k) <= (int(db_lab)+num_include/2):
 			return True
@@ -56,13 +56,13 @@ def computeForwardPasses(nets, alexnet, im, transformer, transformer_alex, resiz
 	t_alex = -1
 
 	imcp = np.copy(im) # for AlexNet
-	
+
 	if im.shape[2] > 1:
 		im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 	if not resize_net:
 		im = cv2.resize(im, (160, 120), interpolation = cv2.INTER_CUBIC)
 	else:
-		transformer = caffe.io.Transformer({'X1':(1,1,im.shape[0],im.shape[1])})	
+		transformer = caffe.io.Transformer({'X1':(1,1,im.shape[0],im.shape[1])})
 		transformer.set_raw_scale('X1',1./255)
 		for net in nets:
 			x1 = net.blobs['X1']
@@ -102,36 +102,36 @@ def computeForwardPasses(nets, alexnet, im, transformer, transformer_alex, resiz
 
 
 def get_prec_recall(net_def_path='proto/calc_deploy.prototxt', net_model_path='model/calc_iter_100000.caffemodel', data_path="test_data/CampusLoopDataset", num_include=7, title='Precision-Recall Curve', resize_net=False, alexnet_proto_path=None, alexnet_weights=None):
-	
+
 	"""
-	Input: 
-	 
+	Input:
+
 	net_def_path='proto/calc_deploy.prototxt': Caffe network definition file
 
 	net_model_path='model/calc_iter_100000.caffemodel': .caffemodel weights file
 
 	data_path="test_data/CampusLoopDataset",: Path to data with corresponding images (with corresponding file names) in a <data-dir>/live and <data-dir>/memory
-	
+
 	resize_net - if True, the net will resize fot he native image size, otherwise, the images
 		will be resized to the native net input size of 120x160
 	"""
 
 	caffe.set_mode_gpu()
 	caffe.set_device(0)
-	
+
 	dbow = None
-	# Check is DBoW2 fork is installed 
+	# Check is DBoW2 fork is installed
 	if path.isfile("ThirdParty/DBoW2/build/pydbow2.so"):
 		sys.path.append("ThirdParty/DBoW2/build")
 		from pydbow2 import PyDBoW2
-		dbow = PyDBoW2("ThirdParty/DBoW2/Vocabulary/ORBvoc.txt")	
-		
+		dbow = PyDBoW2("ThirdParty/DBoW2/Vocabulary/ORBvoc.txt")
+
 	nets = []
 	for m_path in net_model_path:
 		nets.append(caffe.Net(net_def_path,1,weights=m_path))
 
 	database = [] # stored pic descriptors
-	database_labels = [] # the image labels	
+	database_labels = [] # the image labels
 
 	alexnet = None
 	transformer_alex = None
@@ -139,7 +139,7 @@ def get_prec_recall(net_def_path='proto/calc_deploy.prototxt', net_model_path='m
 		db_alex = []
 		alexnet = caffe.Net(alexnet_proto_path,1,weights=alexnet_weights)
 
-		transformer_alex = caffe.io.Transformer({'data':(1,3,227,227)})	
+		transformer_alex = caffe.io.Transformer({'data':(1,3,227,227)})
 		transformer_alex.set_raw_scale('data',1./255)
 		transformer_alex.set_transpose('data', (2,0,1))
 		transformer_alex.set_channel_swap('data', (2,1,0))
@@ -154,9 +154,9 @@ def get_prec_recall(net_def_path='proto/calc_deploy.prototxt', net_model_path='m
 	live_files = [path.join(live_path, f) for f in listdir(live_path)]
 
 	# Use caffe's transformer
-	transformer = caffe.io.Transformer({'X1':(1,1,120,160)})	
+	transformer = caffe.io.Transformer({'X1':(1,1,120,160)})
 	transformer.set_raw_scale('X1',1./255)
-	
+
 	# same HOG params used to train calc
 	hog = cv2.HOGDescriptor((16, 32), (16,16), (16,16), (8,8), 2,1)
 	db_hog = []
@@ -164,30 +164,30 @@ def get_prec_recall(net_def_path='proto/calc_deploy.prototxt', net_model_path='m
 	t_calc = []
 	t_alex = []
 
-	for fl in mem_files:	
+	for fl in mem_files:
 		im = cv2.imread(fl)
 		print "loading image ", fl, " to database"
-		descr, alex_conv3, t_r, t_a = computeForwardPasses(nets, alexnet, im, transformer, transformer_alex, resize_net)		
+		descr, alex_conv3, t_r, t_a = computeForwardPasses(nets, alexnet, im, transformer, transformer_alex, resize_net)
 		t_calc.append(t_r)
 		t_alex.append(t_a)
 		if alexnet is not None:
-			db_alex.append(alex_conv3)		
-		database.append(descr) 		
-	
-		# Use the image sequence number as the label.This assumes that live and memory have the image numbers synce with location, like the Garden Point dataset, or an stereo dataset	
+			db_alex.append(alex_conv3)
+		database.append(descr)
+
+		# Use the image sequence number as the label.This assumes that live and memory have the image numbers synce with location, like the Garden Point dataset, or an stereo dataset
 		database_labels.append(re.match('.*?([0-9]+)$', path.splitext(path.basename(fl))[0]).group(1))
 
 		d_hog = hog.compute(cv2.resize(cv2.cvtColor(im,cv2.COLOR_BGR2GRAY), (160, 120), interpolation = cv2.INTER_CUBIC))
 		db_hog.append(d_hog/np.linalg.norm(d_hog))
-		
+
 		if dbow is not None:
-			dbow.addToDB(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)) 
+			dbow.addToDB(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY))
 
 
 	#print database
 
 	correct = np.zeros((len(nets),len(live_files)),dtype=np.uint8) # the array of true labels of loop closure for precision-recall curve for each net
-	
+
 	scores = np.zeros((len(nets),len(live_files)))  # Our "probability function" that  simply uses 1-l2_norm
 
 	correct_alex = []
@@ -200,20 +200,20 @@ def get_prec_recall(net_def_path='proto/calc_deploy.prototxt', net_model_path='m
 	scores_dbow = []
 
 	k=0
-	for fl in live_files:	
+	for fl in live_files:
 		im_label_k = re.match('.*?([0-9]+)$', path.splitext(path.basename(fl))[0]).group(1)
 		im = cv2.imread(fl)
 
-		descr, alex_conv3, t_r, t_a = computeForwardPasses(nets, alexnet, im, transformer, transformer_alex, resize_net)		
+		descr, alex_conv3, t_r, t_a = computeForwardPasses(nets, alexnet, im, transformer, transformer_alex, resize_net)
 		t_calc.append(t_r)
 		t_alex.append(t_a)
-			
+
 		d_hog = hog.compute(cv2.resize(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY), (160, 120), interpolation = cv2.INTER_CUBIC))
 		d_hog /= np.linalg.norm(d_hog)
 
 		max_sim = -1.0 * np.ones(len(nets))
 		max_sim_alex = -1.0
-		max_sim_hog = -1.0	
+		max_sim_hog = -1.0
 
 		i_max_sim = -1 * np.ones(len(nets), dtype=np.int32)
 		i_max_sim_alex = -1
@@ -222,62 +222,62 @@ def get_prec_recall(net_def_path='proto/calc_deploy.prototxt', net_model_path='m
 		for i in range(len(database)):
 			for j in range(len(nets)):
 				curr_sim = np.dot(descr[j], database[i][j].T) # Normalizd vectors means that this give cosine similarity
-				if curr_sim > max_sim[j]: 
+				if curr_sim > max_sim[j]:
 					max_sim[j] = curr_sim
 					i_max_sim[j] = i
 
 			if alexnet is not None:
-				curr_sim_alex = np.squeeze(np.dot(alex_conv3, db_alex[i].T)) 
-				if curr_sim_alex > max_sim_alex: 
+				curr_sim_alex = np.squeeze(np.dot(alex_conv3, db_alex[i].T))
+				if curr_sim_alex > max_sim_alex:
 					max_sim_alex = curr_sim_alex
 					i_max_sim_alex = i
 
-			curr_sim_hog = np.squeeze(np.dot(d_hog.T,  db_hog[i])) 
-			if curr_sim_hog > max_sim_hog: 
+			curr_sim_hog = np.squeeze(np.dot(d_hog.T,  db_hog[i]))
+			if curr_sim_hog > max_sim_hog:
 				max_sim_hog = curr_sim_hog
 				i_max_sim_hog = i
 
 			if dbow is not None:
 				dbow
 
-		scores[:,k] = max_sim 
-	
-		for j in range(len(nets)):	
-			db_lab = database_labels[i_max_sim[j]]  
+		scores[:,k] = max_sim
+
+		for j in range(len(nets)):
+			db_lab = database_labels[i_max_sim[j]]
 			if check_match(im_label_k, db_lab, num_include):
 				correct[j][k] = 1
 			# else already 0
 			print "Proposed match calc:", im_label_k, ", ", database_labels[i_max_sim[j]], ", score = ", max_sim[j], ", Correct =", correct[j][k]
 
-		if alexnet is not None:		
-			scores_alex.append( max_sim_alex )	
-			db_lab_alex = database_labels[i_max_sim_alex]  
+		if alexnet is not None:
+			scores_alex.append( max_sim_alex )
+			db_lab_alex = database_labels[i_max_sim_alex]
 			if check_match(im_label_k, db_lab_alex, num_include):
 				correct_alex.append(1)
 			else:
-				correct_alex.append(0)			
+				correct_alex.append(0)
 
 			print "Proposed match AlexNet:", im_label_k, ", ", database_labels[i_max_sim_alex], ", score = ", max_sim_alex,", Correct =", correct_alex[-1]
 
-		scores_hog.append( max_sim_hog )	
-		db_lab_hog = database_labels[i_max_sim_hog]  
+		scores_hog.append( max_sim_hog )
+		db_lab_hog = database_labels[i_max_sim_hog]
 		if check_match(im_label_k, db_lab_hog, num_include):
 			correct_hog.append(1)
 		else:
-			correct_hog.append(0)			
+			correct_hog.append(0)
 		print "Proposed match HOG:", im_label_k, ", ", database_labels[i_max_sim_hog], ", score = ", max_sim_hog,", Correct =", correct_hog[-1]
 
-	
+
 		if dbow is not None:
 			i_max_score_dbow, max_score_dbow = dbow.getClosestMatch(cv2.cvtColor(im,cv2.COLOR_BGR2GRAY))
-			scores_dbow.append(max_score_dbow)			
+			scores_dbow.append(max_score_dbow)
 			if check_match(im_label_k, database_labels[i_max_score_dbow], num_include):
 				correct_dbow.append(1)
 			else:
-				correct_dbow.append(0)			
+				correct_dbow.append(0)
 
 			print "Proposed match DBoW2:", im_label_k, ", ", database_labels[i_max_score_dbow], ", score = ", max_score_dbow,", Correct =", correct_dbow[-1]
-	
+
 		print "\n"
 		k += 1
 
@@ -291,10 +291,10 @@ def get_prec_recall(net_def_path='proto/calc_deploy.prototxt', net_model_path='m
 		recalls.append(recall)
 		if len(nets) == 1: # Only get threshold if there's one net. Otherwise we're just coparing them and don't care about a threshold yet
 			perf_prec = abs(precision[:-1] - 1.0) <= 1e-6
-			if np.any(perf_prec):	
+			if np.any(perf_prec):
 				# We want the highest recall rate with perfect precision as our a-priori threshold
 				threshold = np.min(threshold[perf_prec]) # get the largest threshold so that presicion is 1
-	print "\nThreshold for max recall with 1.0 precision = %f" % (threshold) 
+	print "\nThreshold for max recall with 1.0 precision = %f" % (threshold)
 	precision_alex = None
 	recall_alex = None
 	if alexnet is not None:
@@ -316,7 +316,7 @@ def plot(net_def_path='proto/deploy.prototxt', net_model_path=['model/calc.caffe
 	"""
 	Plot the precision recall curve to compare CALC to other methods, or cross validate different iterations of a CALC model.
 	"""
-		
+
 	t0 = time()
 
 	if isinstance(net_model_path, basestring): # someone only inputed a single string, so make it a list so that this code works
@@ -327,20 +327,20 @@ def plot(net_def_path='proto/deploy.prototxt', net_model_path=['model/calc.caffe
 
 	rcParams['font.sans-serif'] = 'DejaVu Sans'
 	rcParams['font.weight'] = 'bold'
-	rcParams['axes.titleweight'] = 'bold'	
-	rcParams['axes.labelweight'] = 'bold'	
-	rcParams['axes.labelsize'] = 'large'	
-	rcParams['figure.figsize'] = [8.0, 4.0]	
-	rcParams['figure.subplot.bottom'] = 0.2	
+	rcParams['axes.titleweight'] = 'bold'
+	rcParams['axes.labelweight'] = 'bold'
+	rcParams['axes.labelsize'] = 'large'
+	rcParams['figure.figsize'] = [8.0, 4.0]
+	rcParams['figure.subplot.bottom'] = 0.2
 	plots = path.join(getcwd(), "plots")
 	rcParams['savefig.directory'] = plots
 	if not path.isdir(plots):
 		makedirs(plots)
 
 	lines = ['-','--','-.',':','.',',','o','v','^','<','>','1','2','3','4','s','p','*','h','H','+','x','D','d','|','_']
-	
+
 	ax = plt.gca()
-	best_auc = -1 
+	best_auc = -1
 	lab_best_auc = ""
 	handles = []
 	for j in range(len(precisions)):
@@ -349,20 +349,20 @@ def plot(net_def_path='proto/deploy.prototxt', net_model_path=['model/calc.caffe
 		label = re.split('.caffemodel',re.split('/',net_model_path[j])[-1])[0]
 		if len(precisions) < 4:
 			label += ' (AUC=%0.2f)' % (curr_auc)
-		
+
 		calc_plt, = ax.plot(r_smooth, p_smooth, '-', label=label, linewidth=2)
 
 		handles.append(calc_plt)
 		if j==0 or curr_auc > best_auc:
 			lab_best_auc = label
-			best_auc = curr_auc	
+			best_auc = curr_auc
 	# Only tell the user the most accurate net if they loaded more than one!
 	if len(precisions) > 1:
 		print("Model with highest AUC:", lab_best_auc)
 
 
 	print "\n\n\n\n"
-	
+
 	plt.xlabel('Recall')
 	plt.ylabel('Precision')
 	plt.ylim([0, 1])
@@ -389,9 +389,9 @@ def plot(net_def_path='proto/deploy.prototxt', net_model_path=['model/calc.caffe
 	leg.draggable()
 	for line in leg.get_lines():
 		line.set_linewidth(3)
-	
+
 	print "Elapsed time = ", time()-t0, " sec"
-	plt.show()	
+	plt.show()
 
 def plot_var_db_size(calc_fl_name, dbow_fl_name):
 	'''
@@ -401,27 +401,27 @@ def plot_var_db_size(calc_fl_name, dbow_fl_name):
 
 	rcParams['font.sans-serif'] = 'DejaVu Sans'
 	rcParams['font.weight'] = 'bold'
-	rcParams['axes.titleweight'] = 'bold'	
-	rcParams['axes.labelweight'] = 'bold'	
-	rcParams['axes.labelsize'] = 'large'	
-	rcParams['figure.figsize'] = [8.0, 4.0]	
-	rcParams['figure.subplot.bottom'] = 0.2	
+	rcParams['axes.titleweight'] = 'bold'
+	rcParams['axes.labelweight'] = 'bold'
+	rcParams['axes.labelsize'] = 'large'
+	rcParams['figure.figsize'] = [8.0, 4.0]
+	rcParams['figure.subplot.bottom'] = 0.2
 	plots = path.join(getcwd(), "plots")
 	rcParams['savefig.directory'] = plots
 	if not path.isdir(plots):
 		makedirs(plots)
 	ax = plt.gca()
 	handles = []
-	calc_fl = open(calc_fl_name, "r")	
+	calc_fl = open(calc_fl_name, "r")
 	calc_str = calc_fl.read().split('\n')
 	m_calc = np.array(re.split(' ', calc_str[0])[:-1]).astype(np.float) # db sizes
 	t_calc = np.array(re.split(' ', calc_str[1])[:-1]).astype(np.float) # mean query times
 	calc_plt, = ax.plot(m_calc, t_calc, label='Ours', linewidth=2)
-	handles.append(calc_plt)	
+	handles.append(calc_plt)
 
 	dbow_plt = None
 	if dbow_fl_name is not None:
-		dbow_fl = open(dbow_fl_name, "r")	
+		dbow_fl = open(dbow_fl_name, "r")
 		dbow_str = dbow_fl.read().split('\n')
 		m_dbow = np.array(re.split(' ', dbow_str[0])[:-1]).astype(np.float) # db sizes
 		t_dbow = np.array(re.split(' ', dbow_str[1])[:-1]).astype(np.float) # mean query times
@@ -440,13 +440,13 @@ def plot_var_db_size(calc_fl_name, dbow_fl_name):
 	leg.draggable()
 	for line in leg.get_lines():
 		line.set_linewidth(3)
-	plt.show()	
+	plt.show()
 
 
 def view_forward_pass(im1_fl, im2_fl, net_def_path='proto/deploy.prototxt', net_model_path='model/Ours.caffemodel'):
 
 	"""
-	View the forward pass of an image through the deployed net. 
+	View the forward pass of an image through the deployed net.
 	"""
 	from matplotlib import rcParams
 	caffe.set_mode_cpu()
@@ -454,7 +454,7 @@ def view_forward_pass(im1_fl, im2_fl, net_def_path='proto/deploy.prototxt', net_
 	im1 = cv2.resize(cv2.cvtColor(cv2.imread(im1_fl), cv2.COLOR_BGR2GRAY), (160,120))
 	im2 = cv2.resize(cv2.cvtColor(cv2.imread(im2_fl), cv2.COLOR_BGR2GRAY), (160,120))
 	# Use caffe's transformer
-	transformer = caffe.io.Transformer({'X1':(1,1,120,160)})	
+	transformer = caffe.io.Transformer({'X1':(1,1,120,160)})
 	transformer.set_raw_scale('X1',1./255)
 	net.blobs['X1'].data[...] = transformer.preprocess('X1', im1)
 	net.forward()
@@ -463,24 +463,11 @@ def view_forward_pass(im1_fl, im2_fl, net_def_path='proto/deploy.prototxt', net_
 	net.blobs['X1'].data[...] = transformer.preprocess('X1', im2)
 	net.forward()
 	relu23 = net.blobs['relu3'].data[0,0,:,:]
-		
+
 	plt.axis('off')
 	plt.imshow(relu13)
-	plt.show()	
+	plt.show()
 
 	plt.axis('off')
 	plt.imshow(relu23)
 	plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
